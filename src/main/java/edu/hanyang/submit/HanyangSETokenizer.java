8ef7d66eeeb113c82853dfe86606f5d82ae01044
김서권 <gimseogwon@gimseogwon-ui-MacBookPro.local>
2022-04-02 17:39:47 +0900

    Build Tokenization Function

--- src/main/java/edu/hanyang/submit/HanyangSETokenizer.java
+++ src/main/java/edu/hanyang/submit/HanyangSETokenizer.java
@@ -1,18 +1,31 @@
 package edu.hanyang.submit;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.tartarus.snowball.ext.PorterStemmer;
 
 import io.github.hyerica_bdml.indexer.Tokenizer;
 
 
 public class HanyangSETokenizer implements Tokenizer {
-
     /**
      * Tokenizer 객체를 생성하고 준비하는 단계
      */
+	private Analyzer analyzer = null;
+	private PorterStemmer s = null;
+	
     @Override
     public void setup() {
         // TODO: your code here...
+    	analyzer = new SimpleAnalyzer();
+    	s = new PorterStemmer();
     }
 
     /**
@@ -23,14 +36,33 @@ public class HanyangSETokenizer implements Tokenizer {
     @Override
     public List<String> split(String str) {
         // TODO: your code here...
-        return null;
+    	List<String> result = new ArrayList<String>();
+    	try {
+    		TokenStream stream = analyzer.tokenStream(null, new StringReader(str));
+    		while(stream.incrementToken()) {
+    			result.add(stemString(
+    					stream.getAttribute(
+    							CharTermAttribute.class).toString()));
+    		}
+    		stream.close();
+    	} catch (IOException e) {
+    		throw new RuntimeException(e);
+    	}
+        return result;
     }
+    
 
     /**
      * Tokenizer 객체를 모두 닫는 단계
      */
     @Override
-    public void clean() {
-        // TODO: your code here...
+    public void clean() { 
+    	analyzer.close();
+    }
+    
+    private String stemString(String word) {
+    	s.setCurrent(word);
+    	s.stem();
+    	return s.getCurrent();
     }
 }
