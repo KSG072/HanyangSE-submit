21080403242555299876bc7edfd9aed3c00c8e69
jae-hun-e <wognskec@hanyang.ac.kr>
2022-05-30 16:49:03 +0900

    create split (writeBlock, insertInternal 회의 필요)

--- src/main/java/edu/hanyang/submit/Block.java
+++ src/main/java/edu/hanyang/submit/Block.java
@@ -1,7 +1,7 @@
 package edu.hanyang.submit;
 
 public class Block {
-    public int parent=0;
+    public long parent=0;
     public int type = 0; //1 is non-leaf, 0 is leaf
     public int nkeys = 0;
     private int max;
@@ -13,7 +13,7 @@ public class Block {
         this.keys = new int[maxKeys];
         this.vals = new int[maxKeys+1];
     }
-    public Block(int p, int t, int n, int maxKeys){
+    public Block(long p, int t, int n, int maxKeys){
         this.parent = p;
         this.type = t;
         this.nkeys = n;
@@ -32,13 +32,10 @@ public class Block {
         int pos;
         for(pos=0; pos<this.nkeys; pos++){
             if(key < this.keys[pos]){
-                for(int j=0; j<pos; j++){
-                    arr[j] = this.keys[j];
-                }
+                System.arraycopy(this.keys, 0, arr, 0, pos);
                 arr[pos] = key;
-                for(int j=pos+1; j<this.nkeys+1; j++){
-                    arr[j] = this.keys[j-1];
-                }
+                if (this.nkeys + 1 - (pos + 1) >= 0)
+                    System.arraycopy(this.keys, pos + 1 - 1, arr, pos + 1, this.nkeys + 1 - (pos + 1));
                 break;
             }
         }
@@ -47,13 +44,10 @@ public class Block {
     }
     private int[] insertVal(int pos, int val){
         int[] values = new int[this.max];
-        for(int i=0; i<pos; i++){
-            values[i] = this.vals[i];
-        }
+        if (pos >= 0) System.arraycopy(this.vals, 0, values, 0, pos);
         values[pos] = val;
-        for(int i=pos+1; i<nkeys+1; i++){
-            values[i] = this.vals[i-1];
-        }
+        if (nkeys + 1 - (pos + 1) >= 0)
+            System.arraycopy(this.vals, pos + 1 - 1, values, pos + 1, nkeys + 1 - (pos + 1));
         return values;
     }
 }
